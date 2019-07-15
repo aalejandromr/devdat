edaff5ff458933327d78384c79f9af2d0ca39c5e
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-07-14 20:10:48 -0400

    Adds major fix on UI and on Desktop UI

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/main.css" />
     <title>DEVDAT</title>
 </head>
+
 <body>
     <div class="row burger-menu">
-            <p><a href="#main_nav" id="access_nav" class="access_aid">Skip to navigation</a></p>
+        <p>
+            <!-- <a href="#main_nav" id="access_nav" class="access_aid">Skip to navigation</a> -->
+        </p>
     </div>
-    <nav id="main_nav">
+    <!-- <nav id="main_nav">
         <ul>
             <li>
                 <a href=""> Home </a>
@@ -29,83 +33,123 @@
                 <a href=""> Contact </a>
             </li>
 
-            <p><a href="#body" id="access_top" class="access_aid">Skip to top</a></p>
+            <p>
+                <a href="#body" id="access_top" class="access_aid">Skip to top</a>
+            </p>
         </ul>
-    </nav>
-    <div class="row main-background">
-        
-    </div>
+    </nav> -->
+    <div class="row main-background"></div>
 
     <div class="row logo">
         <a href="#">
-            <img src="assets/logo/logo-devdat.png" alt="DEVDAT, We Develop that">
+            <img src="assets/logo/logo-devdat.png" alt="DEVDAT, We Develop that" />
         </a>
     </div>
 
     <div class="container mx-auto center">
         <div class="row main-header">
             <div class="s12">
-                <h1 class="sm:text-4xl site-eslogan"> We're innovation </h1>
+                <h1 class="lg:text-6xl sm:text-6xl text-4xl">We're innovation</h1>
             </div>
             <div class="s12">
-                <h3 class="lg:text-2xl"> Let us help you grow your business. </h3>
+                <h3 class="lg:text-2xl sm:text-base text-base">Let us help you grow your business.</h3>
             </div>
         </div>
         <div class="row content-spacing">
-                <div class="the-company-header">
-                    <h2 class="text-3xl"> The company </h2>
-                </div>
-                <hr/>
-                <div class="the-company-info intern-spacing"> 
-                    <p class="text-base"> 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                    </p>
-                </div>
+            <div class="the-company-header">
+                <h2 class="text-3xl">The company</h2>
+            </div>
+            <hr />
+            <div class="the-company-info intern-spacing">
+                <p class="text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
         </div>
 
         <div class="row content-spacing">
-                <div class="what-we-do-header">
-                    <h2 class="text-3xl"> What do we do? </h2>
-                </div>
-                <hr/>
-                <div class="what-we-do-info intern-spacing"> 
-                    <p class="text-base"> 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    </p>
-                </div>
+            <div class="what-we-do-header">
+                <h2 class="text-3xl">What do we do?</h2>
+            </div>
+            <hr />
+            <div class="what-we-do-info intern-spacing">
+                <p class="text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo
+                </p>
+            </div>
         </div>
 
-        <div class="row content-spacing">
-            <div class="services flex items-center">
-                <div class="s6 web-design">
-                    <img class="services-img" src="./assets/icons/WEB_DESIGN_PNG.png" alt="Web Design">
-                    <h5> WEB DESIGN </h5>
-                    <hr/>
+
+        <!-- <div class="services flex items-center">
+                <div class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 web-design">
+                    <img class="services-img" src="./assets/icons/WEB_DESIGN_PNG.png" alt="Web Design" />
+                    <h5>WEB DESIGN</h5>
+                    <hr />
                     <ul>
-                        <li> Wordpress </li>
-                        <li> Sass </li>
-                        <li> Less </li>
-                        <li> CSS </li>
-                        <li> Javascript </li>
+                        <li>Wordpress</li>
+                        <li>Sass</li>
+                        <li>Less</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
                     </ul>
                 </div>
-                <div class="s6 web-applications flex flex-col items-center">
-                    <img class="services-img" src="./assets/icons/WEB_APPLICATIONS_PNG.png" alt="Web Applications">
-                    <h5> WEB APPLICATIONS </h5>
-                    <hr/>
+                <div class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 web-applications flex flex-col items-center">
+                    <img class="services-img" src="./assets/icons/WEB_APPLICATIONS_PNG.png" alt="Web Applications" />
+                    <h5>WEB APPLICATIONS</h5>
+                    <hr />
                     <ul>
-                        <li> ReactJS </li>
-                        <li> AngularJS </li>
-                        <li> Ruby </li>
-                        <li> Ruby on Rails </li>
-                        <li> WebRTC </li>
+                        <li>ReactJS</li>
+                        <li>AngularJS</li>
+                        <li>Ruby</li>
+                        <li>Ruby on Rails</li>
+                        <li>WebRTC</li>
+                    </ul>
+                </div>
+            </div> -->
+        <div class="flex flex-wrap items-center justify-center services-wrapper">
+            <div class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 flex flex-col items-center max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="services-img" src="./assets/icons/WEB_DESIGN_PNG.png" alt="Web Design" />
+                <div class="px-6 py-4">
+                    <div class="team-header font-bold">
+                        <h5>WEB DESIGN</h5>
+                    </div>
+
+                    <hr />
+                    <ul>
+                        <li>Wordpress</li>
+                        <li>Sass</li>
+                        <li>Less</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 max-w-sm flex flex-col items-center rounded overflow-hidden shadow-lg">
+
+
+                <img class="services-img" src="./assets/icons/WEB_APPLICATIONS_PNG.png" alt="Web Applications" />
+                <div class="px-6 py-4">
+                    <div class="team-header font-bold">
+                        <h5>WEB APPLICATIONS</h5>
+                    </div>
+
+                    <hr />
+                    <ul>
+                        <li>ReactJS</li>
+                        <li>AngularJS</li>
+                        <li>Ruby</li>
+                        <li>Ruby on Rails</li>
+                        <li>WebRTC</li>
                     </ul>
                 </div>
             </div>
@@ -113,16 +157,55 @@
 
         <div class="row get-to-know-us content-spacing ">
             <div class="get-to-know-us-header">
-                <h2 class="text-3xl"> Get to know us </h2>
+                <h2 class="text-3xl">Get to know us</h2>
             </div>
-            <hr/>
+            <hr />
             <div class="get-to-know-us-info intern-spacing">
-                <p class="text-base"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                <p class="text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam,
                 </p>
             </div>
-            <div class="flex team-photo-wrapper">
-                <div class="flex items-center s6 team-margin">
+            <div class="flex flex-wrap items-center justify-center team-photo-wrapper">
+                <div
+                    class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 flex flex-col items-center max-w-sm rounded overflow-hidden shadow-lg">
+                    <img class="w-full" src="./assets/pictures/ALE_PHOTO.png" alt="Sunset in the mountains" />
+                    <div class="px-6 py-4">
+                        <div class="team-header font-bold">
+                            <h4> Alejandro Mejia </h4>
+                        </div>
+                        <p class="card-text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                            exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                    <div class="px-6 py-4">
+                        <span
+                            class="card-text-base inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold  mr-2">#2+ years of experience</span>
+                    </div>
+                </div>
+
+                <div
+                    class="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 max-w-sm flex flex-col items-center  rounded overflow-hidden shadow-lg">
+                    <img class="w-full" src="./assets/pictures/EDU_PHOTO.png" alt="Sunset in the mountains" />
+                    <div class="px-6 py-4">
+                        <div class="team-header font-bold">
+                            <h4> Eduardo Mejia </h4>
+                        </div>
+                        <p class="card-text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                            exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                    <div class="px-6 py-4">
+                        <span
+                            class="card-text-base inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold  mr-2">#6+ years of experience</span>
+                    </div>
+                </div>
+                <!-- <div class="flex items-center s6 team-margin">
                     <div class="team flex flex-col ">
                         <img src="./assets/pictures/ALE_PHOTO.png" alt="Jose A Mejia">
                         <div class="team-header">
@@ -133,9 +216,9 @@
                             <p class="text-base"> Web Developer </p>
                         </div>
                     </div>
-                </div>
-    
-                <div class=" s6">
+                </div> -->
+
+                <!-- <div class=" s6">
                     <div class="team flex flex-col items-center">
                         <img src="./assets/pictures/EDU_PHOTO.png" alt="Eduardo Mejia">
                         <div class="team-header">
@@ -146,7 +229,7 @@
                             <p class="text-base"> WebRTC and VoIP Developer </p>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -154,53 +237,58 @@
     <footer>
         <div class="row contact">
             <div class="header intern-spacing">
-                <h2> LETS HELP YOUR BUSINESS! </h2>
+                <h2 class="text-3xl">LETS HELP YOUR BUSINESS!</h2>
             </div>
-            <hr/>
+            <hr />
             <div class="text intern-spacing">
-                <p class="text-base"> Tell us about your project, and get a free quote with an dedicated team. </p>
+                <p class="text-base">
+                    Tell us about your project, and get a free quote with an dedicated
+                    team.
+                </p>
             </div>
             <div class="form">
                 <form action="">
                     <div class="flex flex-col input-wrapper">
                         <label for="name"> </label>
-                        <input name="name" type="text" placeholder="Your name">
+                        <input name="name" type="text" placeholder="Your name" />
                     </div>
 
                     <div class="flex flex-col input-wrapper">
                         <label for="telephone"></label>
-                        <input name="telephone" type="text" placeholder="phone">
-                        
+                        <input name="telephone" type="text" placeholder="phone" />
                     </div>
                     <div class="flex flex-col input-wrapper">
                         <label for="email"></label>
-                        <input type="text" name="email" placeholder="email">
+                        <input type="text" name="email" placeholder="email" />
                     </div>
                     <div class="flex flex-col input-wrapper">
                         <label for="project"></label>
-                        <textarea name="project" id="project" cols="30" rows="10" placeholder="Message"></textarea>    
+                        <textarea name="project" id="project" cols="30" rows="10" placeholder="Message"></textarea>
                     </div>
 
                     <div class="flex flex-row-reverse">
-                        <button class="form-submit"> SEND </button>
+                        <button class="form-submit">SEND</button>
                     </div>
-                    
                 </form>
             </div>
         </div>
 
         <div class="copy-right ">
-            <p class="text-xs"> DEVDAT 2019 All right reserved | + 1 (239) 628- 6079 | <a href=""> info@devdat.io </a> </p>
+            <p class="text-xs">
+                DEVDAT 2019 All right reserved | + 1 (239) 628- 6079 |
+                <a href=""> info@devdat.io </a>
+            </p>
         </div>
     </footer>
 </body>
 <script>
-	var nav = document.getElementById('access_nav'),
-	    body = document.body;
+    var nav = document.getElementById("access_nav"),
+        body = document.body;
 
-	nav.addEventListener('click', function(e) {
-		body.className = body.className? '' : 'with_nav';
-		e.preventDefault();
-	});
+    nav.addEventListener("click", function (e) {
+        body.className = body.className ? "" : "with_nav";
+        e.preventDefault();
+    });
 </script>
+
 </html>
